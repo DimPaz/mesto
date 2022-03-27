8c22c5cc0079d48e23f34090a62529fd3a591f44
Dmitry Pazuxin <dpazuxin@bk.ru>
2022-03-27 23:24:58 +0300

    подключить js

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,11 @@
-console.log ('ddfdfdfdf');
+const profileEditBtn = document.querySelector(".profile__edit-btn");
+const popupWindow = document.querySelector(".popup");
+const popupCloseBtn = popupWindow.querySelector(".popup__close-btn");
+
+function openPopupWindow() {
+  popupWindow.classList.add("popup_opened");
+}
+
+profileEditBtn.addEventListener("click", openPopupWindow);
+
+console.log(document.querySelector(".popup_opened"));
