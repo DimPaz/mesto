9d0a1c1240e491630dd49b10d4bd86cf824318f7
Dmitry Pazuxin <dpazuxin@bk.ru>
2022-03-28 22:43:26 +0300

    что нужно делать?? паника!

--- scripts/index.js
+++ scripts/index.js
@@ -2,14 +2,26 @@ const profileEditBtn = document.querySelector(".profile__edit-btn");
 const popupWindow = document.querySelector(".popup");
 const popupCloseBtn = popupWindow.querySelector(".popup__close-btn");
 
-// открытие popup
+// открытие-закрытие popup
 function openPopupWindow() {
-  popupWindow.classList.add("popup_opened");
+  popupWindow.classList.toggle("popup_opened");
 }
+// открыть popup
 profileEditBtn.addEventListener("click", openPopupWindow);
+// закрыть popup
+popupCloseBtn.addEventListener("click", openPopupWindow);
 
-// закрытие popup
-function closePopupWindow() {
-  popupWindow.classList.remove("popup_opened");
+// закрыть popup на overlay
+function closeOnOverlayClick(event) {
+  //console.log("Event.target", event.target); // самый глубокий элемент, на котором произошло событие.
+  //console.log("Event.carrentTarget", event.currentTarget); // элемент, на котором в данный момент сработал обработчик (до которого «доплыло» событие)
+  if (event.target === event.currentTarget) {
+    openPopupWindow();
+  }
 }
-popupCloseBtn.addEventListener("click", closePopupWindow);
+popupWindow.addEventListener("click", closeOnOverlayClick);
+
+
+let formElement = document.querySelector(".popup__container");
+let nameInput = formElement.querySelector(".popup__container");
+let jobInput = formElement.querySelector(".popup__container");
