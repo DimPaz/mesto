986a7b145158469734bfbaa5edbd1ae2ff21c2c1
Dmirtriy <Dpazuxin@bk.ru>
2022-03-28 13:57:31 +0300

    добавить закрытие popup

--- scripts/index.js
+++ scripts/index.js
@@ -2,11 +2,14 @@ const profileEditBtn = document.querySelector(".profile__edit-btn");
 const popupWindow = document.querySelector(".popup");
 const popupCloseBtn = popupWindow.querySelector(".popup__close-btn");
 
+// открытие popup
 function openPopupWindow() {
   popupWindow.classList.add("popup_opened");
 }
-
 profileEditBtn.addEventListener("click", openPopupWindow);
 
-console.log(document.querySelector(".popup_opened"));
-console.log("dfdfderrer");
+// закрытие popup
+function closePopupWindow() {
+  popupWindow.classList.remove("popup_opened");
+}
+popupCloseBtn.addEventListener("click", closePopupWindow);
