3c54582bba4d2d34dfb6552183c82522149be3be
Dmirtriy <Dpazuxin@bk.ru>
2022-03-29 15:56:40 +0300

    добавить правильную руаботу popup

--- scripts/index.js
+++ scripts/index.js
@@ -1,51 +1,52 @@
+// открытие-закрытие popup
 const profileEditBtn = document.querySelector(".profile__edit-btn");
 const popupWindow = document.querySelector(".popup");
 const popupCloseBtn = popupWindow.querySelector(".popup__close-btn");
+const formElement = document.querySelector(".popup__container");
+const nameInput = formElement.querySelector(".popup__text_input_name");
+const jobInput = formElement.querySelector(".popup__text_input_job");
+const popupSaveBtn = formElement.querySelector(".popup__save-btn");
+const profileName = document.querySelector(".profile__name");
+const profileProfession = document.querySelector(".profile__profession");
 
 // открытие-закрытие popup
 function openPopupWindow() {
-  popupWindow.classList.toggle("popup_opened");
+  nameInput.value = profileName.textContent; //Если пользователь закрывает попап нажав на крестик, то введённые значения не сохраняются
+  jobInput.value = profileProfession.textContent; //Если пользователь закрывает попап нажав на крестик, то введённые значения не сохраняются
+  popupWindow.classList.add("popup_opened");
 }
-// открыть popup
-profileEditBtn.addEventListener("click", openPopupWindow);
-// закрыть popup
-popupCloseBtn.addEventListener("click", openPopupWindow);
+profileEditBtn.addEventListener("click", openPopupWindow); // открыть popup
+
+function closePopupWindow() {
+  popupWindow.classList.remove("popup_opened");
+}
+popupCloseBtn.addEventListener("click", closePopupWindow); // закрыть popup
 
 // закрыть popup на overlay
 function closeOnOverlayClick(event) {
   //console.log("Event.target", event.target); // самый глубокий элемент, на котором произошло событие.
   //console.log("Event.carrentTarget", event.currentTarget); // элемент, на котором в данный момент сработал обработчик (до которого «доплыло» событие)
   if (event.target === event.currentTarget) {
-    openPopupWindow();
+    closePopupWindow();
   }
 }
 popupWindow.addEventListener("click", closeOnOverlayClick);
 
-const formElement = document.querySelector(".popup__container");
-// console.log(formElement);
-const nameInput = formElement.querySelector(".input-name");
-// console.log(nameInput);
-const jobInput = formElement.querySelector(".input-job");
-// console.log(jobInput);
-const popupSaveBtn = formElement.querySelector(".popup__save-btn");
-
-const profileName = document.querySelector(".profile__name");
-// console.log(profileName.textContent);
-const profileProfession = document.querySelector(".profile__profession");
-// console.log(profileProfession.textContent);
-
-function formSubmitHandler(evt) {
-  evt.preventDefault();
+//ввод name и job
+function formSubmitHandler(event) {
+  event.preventDefault();
   profileName.textContent = nameInput.value;
   profileProfession.textContent = jobInput.value;
-  popupSaveBtn.addEventListener("click", openPopupWindow);
+  closePopupWindow();
 }
 formElement.addEventListener("submit", formSubmitHandler);
 
-// function formSubmitHandler (evt) {
-//   evt.preventDefault();
-//   profileName.textContent = nameInput.value;
-//   profileProfession.textContent = jobInput.value;
-//   popupSaveBtn.addEventListener("click", openPopupWindow);
-// }
-// formElement.addEventListener("submit", formSubmitHandler);
+// кнопка лайка
+const cardElement = document.querySelector(".elements");
+const cardLike = document.querySelector(".element__like");
+
+function likeActiv() {
+  cardLike.classList.toggle("element__like_active");
+}
+
+cardLike.addEventListener("click", likeActiv);
