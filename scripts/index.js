e05d8096c2ff2e22dbd4cae31cbdc088f8b08665
Dmitry Pazuxin <dpazuxin@bk.ru>
2022-04-26 01:39:20 +0300

    добавлены ошибки для всех полей

--- scripts/index.js
+++ scripts/index.js
@@ -176,40 +176,62 @@ cardForm.addEventListener("submit", handleCardFormSubmit); //ввод signature 
 // 5 ПР
 
 // Вынесем все необходимые элементы формы в константы
-const formElement = document.querySelector(".form");
+const formElement = document.querySelector(".form"); //html-элемент формы, в которой находится проверяемое поле ввода. Он нужен для поиска элемента ошибки в форме.
 const formInput = formElement.querySelector(".form__input");
 console.log(formInput.id); // "name-input"
 const formError = formElement.querySelector(`.${formInput.id}-error`); //Выбираем элемент ошибки на основе уникального класса
 console.log(formError);
 
 // Функция, которая добавляет класс с ошибкой
-const showInputError = (element, errorMessage) => {
-  element.classList.add("form__input_type_error"); // Показываем красную линию ошибки
-  formError.textContent = errorMessage; //текст браузерных ошибок
-  formError.classList.add("form__input-error_active"); // Показываем сообщение об ошибке
+const showInputError = (formElement, inputElement, errorMessage) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`); // Находим элемент ошибки внутри самой функции
+  inputElement.classList.add("form__input_type_error"); // Показываем красную линию ошибки
+  errorElement.textContent = errorMessage; //текст браузерных ошибок
+  errorElement.classList.add("form__input-error_active"); // Показываем сообщение об ошибке
 };
 
 // Функция, которая удаляет класс с ошибкой
-const hideInputError = (element) => {
-  element.classList.remove("form__input_type_error"); // Скрываем красную линию ошибки
-  formError.classList.remove("form__input-error_active"); // Скрываем сообщение об ошибке
-  formError.textContent = ""; // Очистим ошибку
+const hideInputError = (formElement, inputElement) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`); // Находим элемент ошибки
+  inputElement.classList.remove("form__input_type_error"); // Скрываем красную линию ошибки
+  errorElement.classList.remove("form__input-error_active"); // Скрываем сообщение об ошибке
+  errorElement.textContent = ""; // Очистим ошибку
 };
 
 // Функция, которая проверяет валидность поля
-const isValid = () => {
-  if (!formInput.validity.valid) {
+const isValid = (formElement, inputElement) => {
+  // элемент формы, в которой находится проверяемое поле ввода & проверяемое поле ввода
+  if (!inputElement.validity.valid) {
+    // получаем параметром форму, в которой находится проверяемое поле, и само это поле
     // Если поле не проходит валидацию, покажем ошибку
-    showInputError(formInput, formInput.validationMessage); // formInput & сообщение об ошибке
+    showInputError(formElement, inputElement, inputElement.validationMessage);
   } else {
     // Если проходит, скроем
-    hideInputError(formInput);
+    hideInputError(formElement, inputElement);
   }
 };
 
-formElement.addEventListener("submit", function (evt) {
-  evt.preventDefault(); // Отменим стандартное поведение по сабмиту
-});
+// Добавление обработчиков всем полям формы
+const setEventListeners = (formElement) => {
+  const inputList = Array.from(formElement.querySelectorAll(".form__input")); //делаем массив
+  // Обойдём все элементы полученной коллекции
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener("input", () => {
+      isValid(formElement, inputElement); // передаем форму и проверяемый элемент
+    });
+  });
+};
 
-// Вызовем функцию isValid на каждый ввод символа
-formInput.addEventListener("input", isValid);
+// Добавление обработчиков всем формам
+const enableValidation = () => {
+  const formList = Array.from(document.querySelectorAll(".form"));
+  console.log(formList);
+  formList.forEach((formElement) => {
+    formElement.addEventListener("submit", (evt) => {
+      evt.preventDefault(); // У каждой формы отменим стандартное поведение
+    });
+    setEventListeners(formElement); //Для каждой формы вызовем функцию setEventListeners, передав ей элемент формы
+  });
+};
+
+enableValidation();
